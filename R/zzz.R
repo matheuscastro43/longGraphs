3bf02367a8dd7fdd0c62359128807c738ec8b348
Matheus Castro <mtcastro43@gmail.com>
2021-07-02 16:34:35 -0300

    Startup message

---
+++ R/zzz.R
@@ -0,0 +1,4 @@
+.onLoad <- function(libname, pkgname){
+  packageStartupMessage("You have loaded longGraphs package")
+  packageStartupMessage("By Matheus Oliveira de Castro <mtcastro43@gmail.com>")
+}

d721c941177ef63a71d89876097b7e596fe8ea15
Matheus Castro <mtcastro43@gmail.com>
2021-07-05 17:15:32 -0300

    Development in update

---
+++ R/longGraphs.R
@@ -0,0 +1,8 @@
+#' longGraphs: Longitudinal Graphs
+#'
+#' longGraphs is a package to make elegant longitudinal graphics as easy as they
+#' should be. The entire package was built using ggplot2 functions.
+#'
+#'@docType package
+#'@name longGraphs
+NULL
--- R/spaghetti_plot.R
+++ R/spaghetti_plot.R
@@ -1,14 +1,43 @@
+#' Tidy Spaghetti Plot.
+#'
+#' @description Function to make a Spaghetti Plot using Grammar of Graphics.
+#'
+#' @param dataf A dataframe or tibble.
+#' @param id Variable with the experimental unit index.
+#' @param x Variable to \code{x} axis.
+#' @param y Variable to \code{y} axis (response variable).
+#' @param group Grouping variable (if exists).
+#' @param wrap Formula to separate plot (facet).
+#' @param mfrow Number of rows and columns of facets.
+#' @param funcs Functions to be added to plot.
+#' @param xlab String with the label of \code{x} axis.
+#' @param ylab String with the label of \code{y} axis.
+#' @param legend_title String with the legend title.
+#' @param legend_labels String with the legend labels.
+#'
+#' @return A \code{\link[ggplot2]{ggplot}} object with the plot.
+#'
+#' @author Matheus Castro
+#'     \href{mailto::mtcastro43@@gmail.com}{mtcastro43@@gmail.com}
+#'     (\href{https://orcid.org/0000-0001-7879-7089}{ORCID}).
+#'
+#' @examples
+#' spaghetti_plot(PotRoy, Individual, Age, Distance, Sex, xlab = "Age (years)",
+#'                ylab = "Distance (cm)", legend_title = "Sex",
+#'                legend_labels = c("Female", "Male"), wrap = ~Sex,
+#'                funcs = mean)
+#'
 #' @importFrom magrittr %<>%
 #' @importFrom magrittr %$%
-#' @importFrom tibble deframe
-#' @importFrom rlang as_string
 #' @import dplyr
 #' @importFrom tidyr pivot_longer
 #' @import ggplot2
 #' @importFrom ggthemes theme_calc
+#'
 #' @export
-spaghetti_plot <- function(dataf, id, x, y, group = NULL, grid = NULL,
-                           funcs = NULL, xlab = NULL, ylab = NULL,
+#'
+spaghetti_plot <- function(dataf, id, x, y, group = NULL, wrap = NULL,
+                           mfrow = NULL, funcs = NULL, xlab = NULL, ylab = NULL,
                            legend_title = NULL, legend_labels = NULL) {
   dataf %<>% as_tibble
 
@@ -17,7 +46,7 @@ spaghetti_plot <- function(dataf, id, x, y, group = NULL, grid = NULL,
     aes(x = {{x}}, y = {{y}}) +
     geom_line(aes(group = {{id}}, color = {{group}})) +
     geom_point(aes(group = {{id}}, color = {{group}})) +
-    facet_grid(grid) +
+    facet_wrap(wrap, nrow = mfrow[1], ncol = mfrow[2]) +
     theme_calc() +
     labs(x = if (is.null(xlab)) {
       waiver()
@@ -48,6 +77,7 @@ spaghetti_plot <- function(dataf, id, x, y, group = NULL, grid = NULL,
       summarise(across({{y}}, funcs)) %>%
       pivot_longer(cols = -{{x}})
     p +
-      geom_line(data = auxt, aes(x = {{x}}, y = value, group = name))
+      geom_line(data = auxt, aes(x = {{x}}, y = !!sym("value"),
+                                 group = !!sym("name")))
   } else p
 }
